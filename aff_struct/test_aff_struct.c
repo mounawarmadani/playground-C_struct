fe2795a6c1c246896f4eec69a8c4975619efce19
profThiernesse <44463345+profThiernesse@users.noreply.github.com>
2019-02-01 10:03:07 +0100

    a

--- aff_struct/test_aff_struct.c
+++ aff_struct/test_aff_struct.c
@@ -2,22 +2,6 @@
 #include <stdlib.h>
 #include "aff_struct.h"
 
-struct date {
-	int jour;
-	int mois;
-	int annee;
-};
-
-struct contact {
-	char nom[20];
-	char prenom[20];
-	char adresse[100];
-	char numero[10];
-	char localite[50];
-	int CP;
-	struct date naissance;
-};
-
 int main() {
   struct contact personne;
 
