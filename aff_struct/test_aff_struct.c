0a64aea81d74d3b43f431c486217d80bd6c71888
profThiernesse <44463345+profThiernesse@users.noreply.github.com>
2019-02-01 10:41:12 +0100

    Update test_aff_struct.c

--- aff_struct/test_aff_struct.c
+++ aff_struct/test_aff_struct.c
@@ -1,19 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "aff_struct.h"
 
 int main() {
   struct contact personne;
-	
+
+  fflush(stdin);
   scanf("%s",personne.nom);
+  
+  fflush(stdin);
   scanf("%s",personne.prenom);
+  
+  fflush(stdin);
   fgets(personne.adresse,50,stdin);
+  personne.adresse[strlen(personne.adresse)-1]='\0';
+  
+  fflush(stdin);
   scanf("%s",personne.numero); 
+  
+  fflush(stdin);
   scanf("%s",personne.localite);
+  
+  fflush(stdin);
   scanf("%d",&(personne.CP));
-  
-  printf("->%s ->%d",personne.numero, personne.CP);
-  
+    
   scanf("%d",&(personne.naissance.jour));
   scanf("%d",&(personne.naissance.mois));
   scanf("%d",&(personne.naissance.annee));
