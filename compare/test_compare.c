86ce4f80eecf1df65f6c0b577b929cb0ac094788
profThiernesse <44463345+profThiernesse@users.noreply.github.com>
2019-02-01 13:36:55 +0100

    Update test_compare.c

--- compare/test_compare.c
+++ compare/test_compare.c
@@ -28,9 +28,9 @@ int main(void) {
     int ok=1;
 	
 	struct contact liste[] = {
-								{"Jean","Dupond","","","",0,{12,12,2012}}
 								{"Jean","Dupond","","","",0,{12,12,2012}},
-							};
+								{"Jean","Dupond","","","",0,{12,12,2012}}
+							}
 	
     for (i=0;i<2;i++) {		
 		if (_compare_contact(liste[i],liste[i+1]) == compare_contact(liste[i],liste[i+1])) {
